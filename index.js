1023b937d4cb21bfdff725b5767733a1ed95d095
jayarzuniga <78530445+jayarzuniga@users.noreply.github.com>
2021-03-19 18:26:30 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random()*6+1);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6+1);
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+var heading = document.firstElementChild.lastElementChild.firstElementChild.firstElementChild;
+
+if (randomNumber1 > randomNumber2) {
+    heading.innerHTML = "😁Player 1 Won!🎉";
+} else if (randomNumber1 < randomNumber2){
+    heading.innerHTML = "🎉Player 2 Won!😁";
+} else { heading.innerHTML = "🤷‍♂️Draw!🤷‍♀️";}
